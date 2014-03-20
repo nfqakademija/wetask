faf29d132f16cb0404c4abadd98f1bf713b04af2
gimikus <gimikus@gmail.com>
2014-03-20 20:10:44 +0200

    Aded sonata

---
+++ src/WeTask/WeTaskBundle/Entity/UserRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WeTask\WeTaskBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class UserRepository
+ * @package WeTask\WeTaskBundle\Entity
+ */
+class UserRepository extends EntityRepository
+{
+}
