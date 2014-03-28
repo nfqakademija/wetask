7216734b1fce2b6bdc3ddb8370f2ecb1ac01b09a
Ray <rejus.paukst@gmail.com>
2014-03-28 12:29:28 +0200

    Added empty js functions to help with further developement and once again added fonts to Resources/public

--- src/Nfq/WeDriveBundle/Resources/public/js/maps.js
+++ src/Nfq/WeDriveBundle/Resources/public/js/maps.js
@@ -2,7 +2,8 @@
  * Created by Ray on 14.3.27.
  */
 var currentLocation = new google.maps.LatLng(54.696164, 25.277769);
-var mapCenter = new google.maps.LatLng(54.682167934292465,25.224266052246094);
+var idleCenter = new google.maps.LatLng(54.68901487769897, 25.227699279785);
+var initialisedCenter = idleCenter;          //TODO Set initialised to a new coordinate location
 var map;
 var waypoints = new Array();
 var waypointlim = 8;
@@ -12,14 +13,14 @@ function Initialise() {
     var mapElement = document.getElementById("map-canvas");
 
     var mapOptions = {
-        center: mapCenter,
+        center: idleCenter,
         zoom: 13,
         mapTypeControl: true,
         mapTypeControlOptions: {
             style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
             position: google.maps.ControlPosition.BOTTOM_CENTER
         },
-        panControl: true,
+        pancontrol: true,
         panControlOptions: {
             position: google.maps.ControlPosition.RIGHT_BOTTOM
         },
@@ -41,8 +42,22 @@ function Initialise() {
     map = new google.maps.Map(mapElement, mapOptions);
 
     google.maps.event.addListener(map, 'rightclick', function (event) {
+        console.log('Current waypoints elements:' + waypoints.length);
+        if (waypoints.length == 0) {
+            startRoutePlan();
+        }
         addWaypoint(event);
     });
+    function startRoutePlan() {
+        hideAvailableTrips();
+        showRoutePlanner();
+    }
+
+    function finishRoutePlan() {
+        hideRoutePlanner();
+        showAvailableTrips();
+        waypoints.length = 0;
+    }
 }
 function addWaypoint(event) {
     if (!(waypoints.length > waypointlim)) {
@@ -52,23 +67,28 @@ function addWaypoint(event) {
             map: map
         });
         waypoints.push(waypoint);
-
-//        var entry = createWaypointItem();
-//        entry.text(event.latLng.toString());
-//        $("#waypointdisplay").append(entry);
-    }else{
-//        var entry = createWaypointItem();
-//        entry.text("waypoint limit reached");
-//        $("#waypointdisplay").append(entry);
+    } else {
     }
 };
-//function createWaypointItem(){
-//    var item = $(document.createElement('div'));
-//    item.addClass('panel');
-//    item.addClass('panel-default');
-//    var a = $(document.createElement('a'));
-//    a.addClass('none');
-//    item.append(item);
-//    return item;
-//}
+function hideAvailableTrips() {
+    console.log('trying to hide available trips');
+}
+function showAvailableTrips() {
+    console.log('trying to show available trips');
+}
+function showRoutePlanner() {
+    console.log('trying to show routePlanner');
+    //Add event listeners like added marker or something
+    //Initialise variables
+    //Initialise form
+    map.panTo(initialisedCenter);
+}
+function hideRoutePlanner() {
+    console.log('trying to hide route planner');
+    //Remove event listeners
+    //Clear form variables
+    //Hide form
+    //recenter the map
+    map.panTo(idleCenter);
+}
 google.maps.event.addDomListener(window, 'load', Initialise);
