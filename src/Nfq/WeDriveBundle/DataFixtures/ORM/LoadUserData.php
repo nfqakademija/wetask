08d9ddc66c9a7fb7b7d37dbe66ac096144a544f5
gimikus <gimikus@gmail.com>
2014-04-10 13:23:19 +0300

    Added user-enabled to datafixtures

--- src/Nfq/WeDriveBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Nfq/WeDriveBundle/DataFixtures/ORM/LoadUserData.php
@@ -37,20 +37,20 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
     public function load(ObjectManager $manager)
     {
         $users = array(
-            array('admin', 'test', 'admin@wedrive.dev', 'administrator', 'administrator-1'),
-            array('observer', 'test', 'observer@wedrive.dev', 'observer', 'observer-1'),
-            array('Petras', 'test', 'petras@wedrive.dev', 'user', 'user-1'),
-            array('Jonas', 'test', 'jonas@wedrive.dev', 'user', 'user-2'),
-            array('Viktorija', 'test', 'viktorija@wedrive.dev', 'user', 'user-3'),
-            array('Pranas', 'test', 'pranas@wedrive.dev', 'user', 'user-4'),
-            array('Kestas', 'test', 'kestas@wedrive.dev', 'user', 'user-5'),
-            array('Daiva', 'test', 'daiva@wedrive.dev', 'user', 'user-6'),
-            array('Veronika', 'test', 'veronika@wedrive.dev', 'user', 'user-7'),
-            array('Monika', 'test', 'monika@wedrive.dev', 'user', 'user-8'),
-            array('Ernestas', 'test', 'ernestas@wedrive.dev', 'user', 'user-9'),
-            array('Domas', 'test', 'domas@wedrive.dev', 'user', 'user-10'),
-            array('Tomas', 'test', 'tomas@wedrive.dev', 'user', 'user-11'),
-            array('Markas', 'test', 'markas@wedrive.dev', 'user', 'user-12'),
+            array('admin', 'test', 'admin@wedrive.dev', 'administrator', 'administrator-1',true),
+            array('observer', 'test', 'observer@wedrive.dev', 'observer', 'observer-1',true),
+            array('Petras', 'test', 'petras@wedrive.dev', 'user', 'user-1',true),
+            array('Jonas', 'test', 'jonas@wedrive.dev', 'user', 'user-2',true),
+            array('Viktorija', 'test', 'viktorija@wedrive.dev', 'user', 'user-3',true),
+            array('Pranas', 'test', 'pranas@wedrive.dev', 'user', 'user-4',true),
+            array('Kestas', 'test', 'kestas@wedrive.dev', 'user', 'user-5',true),
+            array('Daiva', 'test', 'daiva@wedrive.dev', 'user', 'user-6',true),
+            array('Veronika', 'test', 'veronika@wedrive.dev', 'user', 'user-7',true),
+            array('Monika', 'test', 'monika@wedrive.dev', 'user', 'user-8',true),
+            array('Ernestas', 'test', 'ernestas@wedrive.dev', 'user', 'user-9',true),
+            array('Domas', 'test', 'domas@wedrive.dev', 'user', 'user-10',true),
+            array('Tomas', 'test', 'tomas@wedrive.dev', 'user', 'user-11',true),
+            array('Markas', 'test', 'markas@wedrive.dev', 'user', 'user-12',true),
         );
 
         $userManager = $this->container->get('fos_user.user_manager');
@@ -61,6 +61,7 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
             $userUser->setPlainPassword($userData[1]);
             $userUser->setEmail($userData[2]);
             $userUser->addRole($userData[3]);
+            $userUser->setEnabled($userData[5]);
 
             $manager->persist($userUser);
 
