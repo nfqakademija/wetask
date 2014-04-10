f7f49d61db86c8ede45b2e82b31945bbdcc60cba
gimikus <gimikus@gmail.com>
2014-04-10 10:35:19 +0300

    Made some test corrections in security.yml Added login form in own layout

--- src/Nfq/UserBundle/NfqUserBundle.php
+++ src/Nfq/UserBundle/NfqUserBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NfqUserBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
