409077bc5efe5fc4626150702f7da3a5d5935afb
gimikus <gimikus@gmail.com>
2014-04-14 17:40:54 +0300

    Login form styles update

--- src/Nfq/WeDriveBundle/Resources/public/js/clock.js
+++ src/Nfq/WeDriveBundle/Resources/public/js/clock.js
@@ -1,29 +1,29 @@
-function startTime()
-        {
-            var today=new Date();
+var Clock = {
+    startTime: function() {
+        var today=new Date();
 
-            var Y=today.getFullYear();
-            var M=today.getMonth();
-            var d=today.getDay();
+        var Y = today.getFullYear();
+        var M = today.getMonth();
+        var d=today.getDay();
 
-            var h=today.getHours();
-            var m=today.getMinutes();
-            var s=today.getSeconds();
-// add a zero in front of numbers<10
-    M=checkTime(M);
-    d=checkTime(d);
+        var h=today.getHours();
+        var m=today.getMinutes();
+        var s=today.getSeconds();
+    // add a zero in front of numbers<10
+        M=this.checkTime(M);
+        d=this.checkTime(d);
 
-    m=checkTime(m);
-    s=checkTime(s);
-    document.getElementById('txt').innerHTML=Y+"-"+M+"-"+d+" "+h+":"+m+":"+s;
-    t=setTimeout(function(){startTime()},500);
-}
+        m=this.checkTime(m);
+        s=this.checkTime(s);
+        document.getElementById('txt').innerHTML=Y+"-"+M+"-"+d+" "+h+":"+m+":"+s;
+        t=setTimeout(function(){Clock.startTime()},500);
+    },
 
-function checkTime(i)
-        {
-            if (i<10)
-            {
+    checkTime: function(i) {
+        if (i < 10) {
             i="0" + i;
-            }
-return i;
+        }
+
+        return i;
+    }
 }
