d53259402de35dc0f188b2f0992ad93665787621
Ray <rejus.paukst@gmail.com>
2014-04-22 17:32:15 +0300

    Working flashbag error field

---
+++ src/Nfq/WeDriveBundle/Resources/public/js/flashbag.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#flash_container").fadeOut(500,function() {
+            $("#flash_container").remove();
+        })
+    }, 3000)
+})
