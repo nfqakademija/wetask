4d97bb8424a3eea46ccc1a489483012d9c9ec13c
gimikus <gimikus@gmail.com>
2014-05-16 10:03:10 +0300

    Done more flexible fixtures. Now you can add fixtures for any time period jus changing $cicleCount in LoadTripData and LoadPassengerData

--- src/Nfq/WeDriveBundle/DataFixtures/ORM/LoadPassengerData.php
+++ src/Nfq/WeDriveBundle/DataFixtures/ORM/LoadPassengerData.php
@@ -45,43 +45,33 @@ class LoadPassengerData extends AbstractFixture implements OrderedFixtureInterfa
             array('trip-12', PassengerState::ST_JOINED, 'user-15'),
             array('trip-3', PassengerState::ST_CANCELED_BY_PASSENGER, 'user-15'),
             array('trip-12', PassengerState::ST_CANCELED_BY_DRIVER, 'user-18'),
-            array('trip-12', PassengerState::ST_JOINED, 'user-16'),
-            array('trip-15', PassengerState::ST_JOINED_DRIVER_ACCEPTED, 'user-10'),
-            array('trip-15', PassengerState::ST_CANCELED_BY_PASSENGER, 'user-15'),
-            array('trip-16', PassengerState::ST_JOINED, 'user-7'),
-            array('trip-17', PassengerState::ST_JOINED, 'user-9'),
-            array('trip-18', PassengerState::ST_JOINED, 'user-11'),
-            array('trip-19', PassengerState::ST_JOINED, 'user-5'),
-
-            array('trip-20', PassengerState::ST_JOINED, 'user-13'),
-            array('trip-20', PassengerState::ST_JOINED, 'user-14'),
-            array('trip-20', PassengerState::ST_JOINED, 'user-15'),
-            array('trip-20', PassengerState::ST_CANCELED_BY_DRIVER, 'user-18'),
-            array('trip-20', PassengerState::ST_JOINED, 'user-16'),
-
-            array('trip-21', PassengerState::ST_JOINED, 'user-8'),
-            array('trip-22', PassengerState::ST_JOINED, 'user-11')
-//        ,
-//
-//            array('trip-15', PassengerState::ST_JOINED_DRIVER_ACCEPTED, 'user-10'),
-//            array('trip-15', PassengerState::ST_CANCELED_BY_PASSENGER, 'user-15'),
-//            array('trip-16', PassengerState::ST_JOINED, 'user-7'),
-//            array('trip-17', PassengerState::ST_JOINED, 'user-9'),
-//            array('trip-18', PassengerState::ST_JOINED, 'user-11'),
-//            array('trip-19', PassengerState::ST_JOINED, 'user-5'),
-//
-//            array('trip-20', PassengerState::ST_JOINED, 'user-13'),
-//            array('trip-20', PassengerState::ST_JOINED, 'user-14'),
-//            array('trip-20', PassengerState::ST_JOINED, 'user-15'),
-//            array('trip-20', PassengerState::ST_CANCELED_BY_DRIVER, 'user-18'),
-//            array('trip-20', PassengerState::ST_JOINED, 'user-16'),
-//
-//            array('trip-21', PassengerState::ST_JOINED, 'user-8'),
-//            array('trip-22', PassengerState::ST_JOINED, 'user-11')
-//
+            array('trip-12', PassengerState::ST_JOINED, 'user-16')
 
         );
+        $passengers = array();
+        $tripCount = 13;
+        $cicleCount = 5;
 
+        for($i = 0; $i <$cicleCount; $i++){
+            $tripND = $tripCount * $i;
+            $passengers[] = array('trip-'.(1+$tripND), PassengerState::ST_JOINED, 'user-11');
+            $passengers[] = array('trip-'.(2+$tripND), PassengerState::ST_JOINED, 'user-5');
+            $passengers[] = array('trip-'.(3+$tripND), PassengerState::ST_JOINED_DRIVER_ACCEPTED, 'user-10');
+            $passengers[] = array('trip-'.(4+$tripND), PassengerState::ST_JOINED, 'user-11');
+            $passengers[] = array('trip-'.(5+$tripND), PassengerState::ST_JOINED, 'user-7');
+            $passengers[] = array('trip-'.(10+$tripND), PassengerState::ST_JOINED, 'user-8');
+            $passengers[] = array('trip-'.(7+$tripND), PassengerState::ST_JOINED, 'user-12');
+            //$passengers[] = array('trip-'.(11+$tripND), PassengerState::ST_JOINED, 'user-2');
+            $passengers[] = array('trip-'.(8+$tripND), PassengerState::ST_JOINED, 'user-5');
+            $passengers[] = array('trip-'.(9+$tripND), PassengerState::ST_JOINED, 'user-9');
+            $passengers[] = array('trip-'.(12+$tripND), PassengerState::ST_JOINED, 'user-13');
+            $passengers[] = array('trip-'.(12+$tripND), PassengerState::ST_JOINED, 'user-14');
+            $passengers[] = array('trip-'.(12+$tripND), PassengerState::ST_JOINED, 'user-15');
+            $passengers[] = array('trip-'.(3+$tripND), PassengerState::ST_CANCELED_BY_PASSENGER, 'user-15');
+            $passengers[] = array('trip-'.(12+$tripND), PassengerState::ST_CANCELED_BY_DRIVER, 'user-18');
+            $passengers[] = array('trip-'.(12+$tripND), PassengerState::ST_JOINED, 'user-16');
+        }
+        
         foreach ($passengers as $passengerData) {
             $passenger = new Passenger();
             $passenger->setTrip($this->getReference($passengerData[0]));
